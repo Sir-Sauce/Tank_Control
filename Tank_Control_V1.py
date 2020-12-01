211501a6eebe3ca861d092efd2cc426f98c73de0
Matthew Morgan <mlmorgan428@gmail.com>
2020-11-30 19:37:12 -0500

    Update Tank_Control_V1.py
    State Logic and Timer Added

--- Tank_Control_V1.py
+++ Tank_Control_V1.py
@@ -14,22 +14,21 @@ window.title("Tank Control GUI")
 window.geometry('600x300')                      #Sets application window size
 window.configure(background = 'light gray')
 
-# --- Initial Variables --- #
+# --- Variables --- #
 
 global Dispense_State; Dispense_State = "Off" 
-radio_values = {"Off" : "1", "On" : "2", "Auto" : "3"}  #radio button index
-flag = " "
-
 global Inlet_State; Inlet_State = 1
-height = 50.0
 global current_height; current_height = 50.0
-mode = "Off"
-
 global Start_Time; Start_Time = time.time()
-print(Start_Time)
-
 global State_1_Interval; State_1_Interval = 2
 global State_2_Interval; State_2_Interval = 0.5
+
+radio_values = {"Off" : "1", "On" : "2", "Auto" : "3"}  #radio button index
+flag = " "
+height = 50.0
+Control_Mode = "Off"
+
+print( "start time: " + str(Start_Time))
 
 
 # --- GUI Functions --- #
@@ -49,83 +48,17 @@ def forget(Widget):
 def retrieve(Widget, x, y):
     Widget.place(x = x, y = y)
     
-def Dispense(dispense_mode):
-    
-    if (Start == True):
-        global Dispense_State
-     
-        if (dispense_mode == "on"):
-            Dispense_Open(True)
-            Dispense_State = "On"
-            forget(Dispense_on); retrieve(Dispense_off, 300, 15)
-            print('dispense on')
-        
-        else: 
-            Dispense_State = "Off"
-            forget(Dispense_off); retrieve(Dispense_on, 200, 15)  
-            print('dispense off')
-            #Dispense_Open(False)
-        #print(dispense_txt) #troubleshoot
-        Dispense_Mode1.config(text = Dispense_State) #must be moved to before the dispense function is called to prevent delay
-    else:
-        pass
-         
-# --- State Functions --- #
-
 def Get_Tank_Height(): 
     
     height = Height_Scalar.get()
-    print("Scalar height " + height)
-
+    print('Scale ht.' + str(height))
 
 
 ########## Dispense and Inlet need to operate from timer functions
 ########## Must store a start time, store button on time, at each interval do something
 ########## Perform while loop, with embedded timer functions to do above
 
-def Dispense_Open():
-    
-    global current_height
-    
-    if (time.time and (current_height > 0.0)):
-        current_height -= 0.5
-        print("current tank height: " + current_height)
-        #time.sleep(1000)
-    
-    else:
-        pass
-
-def Inlet_Open():
-    
-    global current_height
-    
-    
-    ########## Change while loop to if(timer<starttime-interval) then increase ht. 
-    if(time.time() > Start_Time + 1):
-        current_height += 2
-        print("current tank height: " + current_height)
-        
-    # while (current_height < 100.0): 
-    #     current_height += 2
-    #     print("current tank height: " + current_height)
-    #     time.sleep(1000)
-        
-
-############ Timer tasks to update states
-
-# def Timer():
-#     if(time.time() > Start_Time + 1):
-#         if(Dispense_State == 'On' and current_height > 0):
-#             current_height -= 0.5
-#             window.update()
-#         else:
-#             print('Cannot Dispense - Tank is Empty')
-#         #if()
-
-
-
-
-
+# --- State Functions --- #
 
 def Warning_Status():
     
@@ -145,62 +78,62 @@ def Warning_Status():
 
 # --- State Logic --- #
 
-#Two seperate state logic functions for "two" concurrently active states
+def Dispense(Dispense_Button):
+    global Dispense_State
+    global Dispense_Mode
+    if (Start == True):
+        
+        if (Dispense_Button == "On"):
+            forget(Dispense_on); retrieve(Dispense_off, 300, 15)
+            print('dispense on')
+            Dispense_State = 'On'; return Dispense_State
+            window.update()
+        
+        else: 
+            forget(Dispense_off); retrieve(Dispense_on, 200, 15)  
+            print('dispense off')
+            Dispense_State = 'Off'; return Dispense_State
+            window.update()
+
+        Dispense_Mode1.config(text = Dispense_State) #must be moved to before the dispense function is called to prevent delay
+
 
 def Inlet_State_Logic(Inlet_State):             
-    
+    global Active_Inlet_State
     if(Start == True):
         #Pre-process 
         print("executing state logic")
         print(Inlet_State)
-        Get_Tank_Height()                           #get target tank height from scale      
-        Warning_Status() 
         
         #Inlet status
         if(Inlet_State == 1):                       #inlet closed
-            print("off-inlet closed")
-            pass
-        
+            Active_Inlet_State = 1
+            Control_Mode = "Off"
+            print("off-inlet closed" + str(Active_Inlet_State))
+                    
         elif(Inlet_State == 2):                     #inlet open
-            Inlet_Open()
-            print("on-inlet open")
+            Active_Inlet_State = 2
+            Control_Mode = "On"
+            print("on-inlet open" + str(Active_Inlet_State))
         
         elif(Inlet_State == 3):
+            Control_Mode = "Auto"
             print("Auto mode")
-            
             if(current_height <= (height - 0.5)):   #inlet auto mode
-                Inlet_Open()
-                print("auto-inlet open")
-            
+                Active_Inlet_State = 2
+                print("auto-inlet open" + str(Active_Inlet_State))
             else:
-                Inlet_State = 1
-                print("auto-inlet closed")
+                Active_Inlet_State = 1
+                print("auto-inlet closed" + str(Active_Inlet_State))
         else:
             pass
+        return Active_Inlet_State; return Control_Mode
+
     else:
+        Active_Inlet_State = 0 
+        return Active_Inlet_State
         print("Not Started")
-                                   #Check tank status for Warnings
 
-def Dispense_State_Logic(Dispense_State):
-    
-    if(Start == True):
-        #Dispense Status
-        
-        if (Dispense_State == "On"):                #dispense on
-            Dispense_Open()    
-            Dispense("on")
-        
-        else:       
-            Dispense("off")                         #dispense off   
-            pass                       
-    else:
-        pass
-    Warning_Status()                                #Check tank status for Warnings
-
-#def Timer_Func():
-#    Dispense_State_Logic(Dispense_State)
-#    Inlet_State_Logic(Inlet_State)
-#    window.after(1000, Timer_Func())
 
 # --- GUI Buttons --- #
    
@@ -210,10 +143,10 @@ Exit_Button.place(x = 20, y = 20)
 Start_Button = tk.Button(text='Start', command = Start)
 Start_Button.place(x = 20, y = 50)
 
-Dispense_on = tk.Button(text='Dispense: On', command = lambda: Dispense_State_Logic("On"))
+Dispense_on = tk.Button(text='Dispense: On', command = lambda: Dispense("On"))
 Dispense_on.place(x = 200, y = 15)
 
-Dispense_off = tk.Button(text='Dispense: Off', command = lambda: Dispense_State_Logic("Off"))
+Dispense_off = tk.Button(text='Dispense: Off', command = lambda: Dispense("Off"))
 Dispense_off.place(x = 300, y = 15)
 
 # --- GUI Control Labels --- #
@@ -245,7 +178,7 @@ Scale.place(x = 300, y = 330)
 # --- GUI Output Labels --- #
 #for displaying continuously refreshed parameters
 
-Control_label1 = tk.Label(text= mode, width = 5)
+Control_label1 = tk.Label(text= Control_Mode, width = 5)
 Control_label1.place(x = 350, y = 80)
 
 Tank_Height1 = tk.Label(text= current_height, width = 5)
@@ -261,16 +194,77 @@ Dispense_Mode1.place(x = 350, y = 200)
 
 Height_Scalar = tk.Scale(window, bg = 'light grey', variable = height, from_ = 100, to = 0, orient = 'vertical') 
 Height_Scalar.place(x = 475, y = 140)
-#Scale.config(text = "Tank Height")
 Height_Scalar.set(50.0)
 
 #establish radio button layout
+def Radio_Button():
+    for (text, value) in radio_values.items(): 
+        tk.Radiobutton(window, text = text, variable = Inlet_State,            
+        value = value, width = 10, command = lambda: Inlet_State_Logic(Inlet_State)).place(x = 20, y = 120+int(value)*20)
+        print('this is from radio button' + str(Inlet_State))
 
-for (text, value) in radio_values.items(): 
-    tk.Radiobutton(window, text = text, variable = Inlet_State,
-    value = value, width = 10, command = lambda: Inlet_State_Logic(Inlet_State)).place(x = 20, y = 120+int(value)*20)
 
 # --- Begin GUI Mainloop --- #    
 
-#window.after(1000, Timer_Func())
-window.mainloop()
+    
+############ Timer tasks to update states
+while(2>1):
+    window.update()
+    print('it is on')
+    print('curernt ht.: ' + str(current_height))
+    print(Dispense_State)
+    Radio_Button()
+    
+    Dispense(Dispense_State)
+    Inlet_State_Logic(Inlet_State)
+    
+    if(time.time() > Start_Time + 1):
+        
+        if(Dispense_State == 'On' and current_height > 0):
+            current_height -= 0.5
+        else:
+            print('Cannot Dispense - Tank is Empty')
+    
+    Inlet_State_Logic(Inlet_State)
+    
+    if(Active_Inlet_State == 1):
+        current_height += 2
+        print("current tank height: " + current_height)
+    
+    if(Dispense_State == 'On'):
+        current_height -= 0.5
+        print("current tank height: " + str(current_height))
+        
+    Start_Time = time.time()
+    window.mainloop()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
