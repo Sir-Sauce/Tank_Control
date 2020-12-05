e4c4d7b4d48afdc674c27b910f3da21a312e9749
Sir-Sauce <austintc180@gmail.com>
2020-12-05 15:19:19 -0500

    Tank Level LEDs
    Started adding functionality to Tank LED height tracking.

--- Tank_Control_V1/Tank_Control_V1.ino
+++ Tank_Control_V1/Tank_Control_V1.ino
@@ -11,16 +11,27 @@ char serialData;
 //int Task1State = 0;
 //int Task2State = 0;
 int WarningFlag = 2;
+int HeightFlag = 3;
 
 #define WarningHigh 0;
 #define WarningLow 1;
 #define WarningOff 2;
+#define TankEmpty 3;
+#define Tank20 4;
+#define Tank40 5;
+#define Tank60 6; 
+#define Tank80 7; 
+#define Tank100 8;
+
 
 void setup() {
   Serial.begin(38400);                //initializes serial communication
   for (int i =0;i < 8; i++) {
       pinMode(ledPin[i], OUTPUT);     //initializing all ledPins as OUTPUT
+  for (int i =0;i < 8; i++) {
+      digitalWrite(ledPin[i], LOW);
   }
+}
 }
 
 void loop() {
@@ -33,15 +44,34 @@ void loop() {
   switch (serialData) {             //Sets Task State based on serial data
     case '0':
       WarningFlag = WarningHigh;
-      break;
+      //break;
     case '1':
       WarningFlag = WarningLow;
-      break;
+      //break;
     case '2':
       WarningFlag = WarningOff;
+      //break;
+    case '3':
+      HeightFlag = TankEmpty;
+      //break;
+    case '4':
+      HeightFlag = Tank20;
+      //break;
+    case '5':
+      HeightFlag = Tank40;
+      //break;
+    case '6':
+      HeightFlag = Tank60;
+      //break;
+    case '7':
+      HeightFlag = Tank80;
+      //break;
+    case '8':
+      HeightFlag = Tank100;
       break;
   }
    SetWarning();
+   HeightIndicator();
 }
 
 
@@ -62,15 +92,42 @@ void loop() {
 //}
 
 void SetWarning() {
-  switch (WarningFlag) {             //Task 1 Timing
-    case 0:                         //Task 1 State On
+  switch (WarningFlag) {            //Switch case for warning state
+    case 0:                         //High level warning
       digitalWrite(7, HIGH);
       digitalWrite(8, HIGH);
-    case 1:                         //Task 1 State Off
+    case 1:                         //low level warning
       digitalWrite(7, HIGH);
-    case 2:
+    case 2:                         //nominal tank level
       digitalWrite(7, LOW);
       digitalWrite(8, LOW);
+  }
+}
+
+void HeightIndicator() {
+  switch (HeightFlag) {             //Switch case for warning state
+    case 3:                         //Tank 0%
+      for (int i =0;i < 5; i++) {
+        digitalWrite(ledPin[i], LOW); 
+      }    
+    case 4:                         //Tank <20%
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+    case 5:                         //Tank <40%
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+    case 6:                         //Tank <60%
+      digitalWrite(2, HIGH);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+    case 7:                         //Tank <80%
+      digitalWrite(5, HIGH);
+      digitalWrite(6, LOW);
+    case 8:                         //Tank >= 80%
+      for (int i =0;i < 5; i++) {
+        digitalWrite(ledPin[i], HIGH); 
+      }
   }
 }
  
